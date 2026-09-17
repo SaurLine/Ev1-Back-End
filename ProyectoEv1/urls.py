566bd3b75bcabb2f4b151d2f14a5c44635f0afa2
SaurLine <rodrigohak372@gmail.com>
2026-09-17 09:46:06 -0300

    Proyecto terminado

--- ProyectoEv1/urls.py
+++ ProyectoEv1/urls.py
@@ -19,9 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app1/", include("App1.urls"))
-
-
-
-  
+    path("app1/", include("App1.urls")),
+    path("app2/", include("App2.urls")) 
 ]
