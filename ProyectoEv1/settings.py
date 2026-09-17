1cd63055aed07da69686583d84a041ab3fecb43d
SaurLine <rodrigohak372@gmail.com>
2026-09-17 09:25:37 -0300

    App2 terminada

--- ProyectoEv1/settings.py
+++ ProyectoEv1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App1',
+    'App2',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
